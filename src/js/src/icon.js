dccc141dd769fe8f8a35af8a25be44820918f288
Will Potter <will@wpotter.me>
2012-08-07 10:56:45 -0400

    changed the icon

--- src/js/src/icon.js
+++ src/js/src/icon.js
@@ -2,9 +2,9 @@ S.Icon = L.Icon.extend({
 	options: {
 	    iconUrl: '/build/images/arrow.png',
 	    shadowUrl: null,
-	    iconSize: new L.Point(25, 29),
+	    iconSize: new L.Point(31, 40),
 	    shadowSize: null,
-	    iconAnchor: new L.Point(13, 15),
+	    iconAnchor: new L.Point(15, 20),
 	    popupAnchor: new L.Point(-3, -10)
     }
 });
